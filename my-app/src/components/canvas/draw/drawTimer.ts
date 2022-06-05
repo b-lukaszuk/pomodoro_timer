6b68551cf498a373b3bb5df94ac45fc8579609cc
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-05 23:06:31 +0200

    dodanie funkcji rysujacej timer

---
+++ my-app/src/components/canvas/draw/drawTimer.ts
@@ -0,0 +1,27 @@
+import drawArc from "./drawArc";
+import { formatTime } from "../../../utils/format";
+import numsToDeg from "../../../utils/numToDeg";
+import minsToHrMinsSecs from "../../../utils/minsToHrsMinsSecs";
+import ITime from "../../../interfaces/ITime";
+import degreeToRadian from "../../../utils/degreeToRadian";
+
+function drawTimer(canv: HTMLCanvasElement, ctx: CanvasRenderingContext2D,
+    minsStart: number, minsLeft: number): void {
+
+    let fontSize: number = parseInt((canv.height / 8).toFixed(0));
+    let time: ITime = minsToHrMinsSecs(minsLeft);
+
+    drawArc(ctx, canv.height / 2, canv.width / 2, 240, 360, 10, "lightgrey");
+    drawArc(ctx, canv.height / 2, canv.width / 2, 240,
+        360 * (1 - minsLeft / minsStart), 10, "red");
+
+    ctx.font = `${fontSize}px Arial`;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = (minsLeft === 0) ? "red" : "grey";
+    ctx.fillText(formatTime(time.hrs, time.mins, time.secs), (canv.width / 2),
+        (canv.height - fontSize));
+
+}
+
+export default drawTimer;
