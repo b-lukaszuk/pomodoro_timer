73933e3414b4562310f03b1d245f022d36f6fb6b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-05 21:57:51 +0200

    dodano dodatkowe komponenty TextInput i Button

---
+++ my-app/src/components/TextInput.tsx
@@ -0,0 +1,38 @@
+import React, { ChangeEvent, ReactElement } from "react";
+
+import "./TextInput.css";
+
+interface handlerFn {
+    (event: ChangeEvent<HTMLInputElement>): void
+}
+
+interface Props {
+    name: string;
+    changeHandler: handlerFn;
+    label: string;
+    pattern?: string;
+    placeholder?: string;
+    value: number;
+}
+
+const InputElt: React.FC<Props> = (props): ReactElement<HTMLInputElement> => {
+    const nameIn: string = props.name;
+    const changeHandlerIn: handlerFn = props.changeHandler;
+    const labelIn: string = props.label;
+    const patternIn: string = props.pattern ? props.pattern : ".*";
+    const placeholderIn: string = props.placeholder ? props.placeholder : "";
+    const valueIn: number = props.value;
+
+    return (
+        <div>
+            <span>{labelIn}: &nbsp;</span>
+            <input name={nameIn} type="text"
+                pattern={patternIn} placeholder={placeholderIn}
+                value={valueIn} maxLength={4} size={4}
+                onChange={changeHandlerIn} />
+        </div>
+    )
+
+}
+
+export default InputElt;
