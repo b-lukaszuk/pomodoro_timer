c4f5ce8478e46299e67d0ff4ff9edcfd9cbca821
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-06 14:26:08 +0200

    kosmetyka, dod. sekundnik do Timera (przy samodzielnym wyswietlaniu)

--- my-app/src/components/canvas/draw/drawTimer.ts
+++ my-app/src/components/canvas/draw/drawTimer.ts
@@ -1,6 +1,8 @@
 import drawArc from "./drawArc";
+import drawPieceOfPie from "./drawPieceOfPie";
 import drawText from "./drawText";
 import { formatTime } from "../../../utils/format";
+import numsToDeg from "../../../utils/numToDeg";
 import secsToHrMinsSecs from "../../../utils/secsToHrsMinsSecs";
 import ITime from "../../../interfaces/ITime";
 
@@ -14,6 +16,11 @@ function drawTimer(canv: HTMLCanvasElement, ctx: CanvasRenderingContext2D,
     drawArc(ctx, canv.height / 2, canv.width / 2, 240, 360, 10, "lightgrey");
     drawArc(ctx, canv.height / 2, canv.width / 2, 240,
         360 * (1 - secsLeft / secsStart), 10, "red");
+
+    if (!displDigitsAtBottom) {
+        drawPieceOfPie(ctx, canv.height / 2, canv.width / 2, 200, 0,
+            numsToDeg(secsStart - secsLeft, 60), "rgba(255, 0, 0, 0.1)");
+    }
 
     drawText(canv, ctx, formatTime(time.hrs, time.mins, time.secs),
         color, displDigitsAtBottom);
